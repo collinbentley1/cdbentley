9184d1e02e1e011114bfa40d62973d8ec1bdd83a
collinbentley1 <collin.bentley@me.com>
2026-09-06 13:10:41 -0400

    chore: pin platform consumers to verified 52cb2de

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -1,5 +1,5 @@
 module "bootstrap" {
-  source = "github.com/collinbentley1/platform//terraform/modules/bootstrap?ref=6b745b98da093b83683109cdc9f0bb6f5ad24eaf"
+  source = "github.com/collinbentley1/platform//terraform/modules/bootstrap?ref=52cb2dea765ead93f204ee4a5bd884b9fbb0f13a"
 
   app                         = "cdbentley"
   project_id                  = var.project_id
@@ -10,7 +10,7 @@ module "bootstrap" {
   github_owner                = var.github_owner
   github_repo                 = var.github_repo
   github_repository_id        = var.github_repository_id
-  active_workflow_sha         = "6b745b98da093b83683109cdc9f0bb6f5ad24eaf"
+  active_workflow_sha         = "52cb2dea765ead93f204ee4a5bd884b9fbb0f13a"
 
   manage_automatic_default_service_account_grants_policy = var.manage_automatic_default_service_account_grants_policy
   runtime_description                                    = "Runtime identity for the cdbentley Cloud Run services."
